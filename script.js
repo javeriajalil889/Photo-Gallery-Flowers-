3ef019c112d4ce55991904532710f45506eeb80d
Javeria Jalil <60556868+javeriajalil889@users.noreply.github.com>
2023-02-11 23:40:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+h1{
+  font-family: 'Trebuchet MS', sans-serif;
+  font-size:100px;
+  background-color:#E0C7AD;
+  color:white;
+  text-align:center;
+  margin:40px;
+  padding:50px;
+}
+h2{
+  font-family: 'Trebuchet MS', sans-serif;
+  font-size:80px;
+  background-color:white;
+  color:#E0C7AD;
+  text-align: center;
+  border: solid #E0C7AD;
+  margin-left:300px;
+  margin-right:300px;
+  padding:30px
+}
+
+p{
+  font-family: 'Trebuchet MS', sans-serif;
+  font-size:60px;
+  text-align:center;
+  color: #E0C7AD;
+  font-style:italic;
+}
+
+img{
+  padding: 30px;
+  height: 700px;
+  width: 700px; 
+  object-fit:cover;
+}
+
+.yellow{
+  background-color: #f2e9df;
+  padding:10px;
+  margin:5px
+}
+.red{
+  background-color: #f2e9df;
+  padding:10px;
+  margin: 5px
+}
